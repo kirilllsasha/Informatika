10a28c2c6023e69a033df73299735e3832c596d2
kirilllsasha <leonteva841@gmail.com>
2024-02-28 00:03:04 +0300

    Create Program.cs

---
+++ жомашнее Иордатий/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace жомашнее_Иордатий
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1
+            //Console.Write("Введите размерность массива:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] mass = new int[n];
+            //Random random = new Random();
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = random.Next(-10, 10);
+            //}
+            //foreach (int elem in mass)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            //int index = 0;
+            //for(int i=0;i<mass.Length;i++)
+            //{
+            //    if (mass[0] == mass[i])
+            //    {
+            //        index = i;
+            //    }
+            //}
+            //Console.WriteLine($"Индекс похожего числа равняется:{index}");
+
+            //Задача 2
+            //Console.Write("Введите размерность массива:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] mass = new int[n];
+            //Random random = new Random();
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = random.Next(-10, 150);
+            //}
+            //foreach(int elem in mass)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            //a
+            //for(int i=0;i<mass.Length;i++)
+            //{
+            //    if (mass[i]>=0)
+            //    {
+            //        Console.WriteLine($"Неотрицательные элементы:{mass[i]}");
+            //    }
+            //}
+            //б
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    if (mass[i] <=100)
+            //    {
+            //        Console.WriteLine($"Элементы не превышающие число 100:{mass[i]}");
+            //    }
+            //}
+            //в
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    if (mass[i]%2==0)
+            //    {
+            //        Console.WriteLine($"Четные элементы:{mass[i]}");
+            //    }
+            //}
+
+            //Задача 3
+            //int[] mass = new int[] { 5, -3, 4, -8, -9, 1, 9, 6, -3, 4, 8, 2 };
+            //foreach(int elem in mass)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            //int count = 0;
+            //for(int i=0;i<mass.Length;i++)
+            //{
+            //    if (mass[i]<0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine($"Кол-во отрицательных элементов:{count}");
+
+            //Задача 4
+            //Console.Write("Введите размерность массива:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] mass = new int[n];
+            //Random random = new Random();
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = random.Next(-30, 30);
+            //}
+            //foreach (int elem in mass)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            //int count = 0;
+            //int average = 0;
+            //for(int i=0;i<mass.Length;i++)
+            //{
+            //    if (mass[i] % 2 == 1 || mass[i]%2==-1)
+            //    {
+            //        count++;
+            //        average = average + mass[i];
+            //    }
+            //}
+            //Console.WriteLine($"Среднее арифметическое:{average/count}");
+
+            //Задача 5
+            //Console.Write("Введите размерность массива:");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] mass = new int[n];
+            //Random random = new Random();
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = random.Next(-30, 30);
+            //}
+            //foreach (int elem in mass)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            //for (int i = 0; i < mass.Length; i++)
+            //{
+            //    mass[i] = mass[i] * 5;
+            //}
+            //foreach(int elem in mass)
+            //{
+            //    Console.WriteLine($"{elem}");
+            //}
+            Console.ReadKey();
+        }
+    }
+}
